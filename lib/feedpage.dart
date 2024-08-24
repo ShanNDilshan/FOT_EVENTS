d34c79937eac7f6a3906daec016d5130882914de
shanuka dilshan <shanndilshanx@gmail.com>
2024-08-24 17:07:41 +0530

    8-24-2024

--- lib/feedpage.dart
+++ lib/feedpage.dart
@@ -1,37 +1,17 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class FeedPage extends StatelessWidget {
-  final CollectionReference organizations =
-      FirebaseFirestore.instance.collection('organizations');
+class FeedPage extends StatefulWidget {
+  const FeedPage({super.key});
 
   @override
+  State<FeedPage> createState() => _FeedPageState();
+}
+
+class _FeedPageState extends State<FeedPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Organizations")),
-      body: StreamBuilder(
-        stream: organizations.snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData)
-            return Center(child: CircularProgressIndicator());
-
-          final docs = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              final data = docs[index].data() as Map<String, dynamic>;
-              return Card(
-                child: ListTile(
-                  leading: Image.network(data['orgDp']),
-                  title: Text(data['orgName']),
-                  subtitle: Text(data['orgDiscription']),
-                ),
-              );
-            },
-          );
-        },
-      ),
+    return const Column(
+      children: [],
     );
   }
 }
